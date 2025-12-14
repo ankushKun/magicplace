34946d90ce27ea6722c6dc3e0ebbf0b975d8d11e
ankushKun <ankush4singh@gmail.com>
2025-12-15 01:24:48 +0530

    bubblewrap buildtool

---
+++ bubblewrap/app/src/main/java/pixels_earth/ankush/one/DelegationService.java
@@ -0,0 +1,18 @@
+package pixels_earth.ankush.one;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
